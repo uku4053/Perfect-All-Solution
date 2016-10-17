aab1f11020856b83d07fd688b5980bb1e1c0ca6c
parkyunsuk <uni7761000@naver.com>
2016-10-17 23:34:55 +0900

    ch05

---
+++ Ch05/Prj01/artihop.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+	printf("3 + 4==> %d\n", 3 + 4);
+	printf("3.4 - 4.3 ==> %f\n", 3.4 - 4.3);
+	printf("3.4 * 4.3==> %f\n", 3.4 * 4.3);
+	printf("10/3 ==> %d\n", 10 / 3);
+	printf("10.0 * 3.0 ==>", 10.0 * 3.0);
+	printf("10 %% 3 ==> %d\n", 10 % 3);
+
+	return 0;
+}
